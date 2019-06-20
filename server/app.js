e911aae91e3353f933310a17fb7db1f1c782fa76
noviirna <novi.irnawati@gmail.com>
2019-06-20 19:28:55 +0700

    first

--- server/app.js
+++ server/app.js
@@ -1,28 +1,23 @@
-if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'){
-    require('dotenv').config()
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const port = 3000
-// const userRoutes = require('./routes/userRoutes')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const routes = require('./routes');
-const morgan = require('morgan');
-var bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 3000;
 
+const cors = require("cors");
+const routes = require("./routes");
+const morgan = require("morgan");
 
-mongoose.connect('mongodb://localhost/kartu-lebaran', { useNewUrlParser: true })
-app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: false }))
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.json({limit: '1mb'}));
-app.use(express.json())
-app.use(cors())
+app.use(express.json({ limit: "2mb" }));
+app.use(cors());
 
-app.use('/', routes)
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
